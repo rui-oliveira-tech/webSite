ad666ef847f595364bdd623228bd2689b155a516
rui1999 <rui.f.oliveira.95@gmail.com>
2022-07-28 02:17:15 +0200

    edit links in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,13 +5,13 @@ export function Footer() {
     return (
         <footer >
             <div>
-                <a className="icon" target="_blank" rel="noreferrer" href="https://www.instagram.com/r.u.i_o.l.i.v.e.i.r.a/"><i className="fab fa-instagram" /></a>
+                <a className="icon" target="_blank" rel="noreferrer" href="https://www.instagram.com/rui_oliveira_tech/"><i className="fab fa-instagram" /></a>
 
                 <a className="icon" target="_blank" rel="noreferrer" href="https://wa.me/message/NX352LLKCXBFH1"><i className="fab fa-whatsapp" /></a>
 
                 <a className="hireMe hireMe_btn  from-center" target="_blank" rel="noreferrer" href="mailto:hire@rui-oliveira.com">Hire Me</a>
 
-                <a className="icon" target="_blank" rel="noreferrer" href="https://github.com/rui1999"><i className="fab fa-github" /></a>
+                <a className="icon" target="_blank" rel="noreferrer" href="https://github.com/rui-oliveira-tech"><i className="fab fa-github" /></a>
 
                 <a className="icon" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/rui-tech/"><i className="fab fa-linkedin-in" /></a>
 
