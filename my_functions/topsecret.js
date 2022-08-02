97d7cf7568dcfa0560d603df7c446d301f3b1bcc
rui1999 <rui.f.oliveira.95@gmail.com>
2022-08-02 23:35:56 +0200

    add server

--- my_functions/topsecret.js
+++ my_functions/topsecret.js
@@ -2,12 +2,13 @@ const fs = require('fs')
 
 exports.handler = async () => {
 
-    const contents = fs.readFileSync('./vcard.vcf', {encoding: 'base64'});
+   /*  const contents = fs.readFileSync('./vcard.vcf', {encoding: 'base64'}); */
+    const { greeting } = require(`./vcard.vcf`);
 
     return {
         statusCode: 200,
         headers: {'Content-type' : 'text/vcard'},
-        body: contents,
+        body: greeting,
         isBase64Encoded : true,
     }
 }
