827c32865fe2a28332d8bc1ef659f1681affdf31
rui1999 <rui.f.oliveira.95@gmail.com>
2022-08-06 01:21:40 +0200

    vcard info

--- routes/vcard.js
+++ routes/vcard.js
@@ -22,14 +22,47 @@ function getVcard(type, filename) {
   const outputFile = `${__dirname}/${filename}`;
 
   // Set common properties
+  vCard.firstName = 'Rui';
+  vCard.lastName = 'Oliveira';
+  vCard.gender = 'M';
+  vCard.url = 'https://www.rui-oliveira.com/';
+  vCard.socialUrls['facebook'] = 'https://...';
+  vCard.socialUrls['linkedIn'] = 'https://...';
+  vCard.socialUrls['twitter'] = 'https://...';
+  vCard.socialUrls['flickr'] = 'https://...';
+  vCard.socialUrls['custom'] = 'https://...';
+  vCard.logo.attachFromUrl('https://avatars2.githubusercontent.com/u/5659221?v=3&s=460', 'JPEG');
 
   switch (type) {
     case "work":
       // Set properties that will only be in "work" card
+      vCard.namePrefix = 'Mr.';
+      vCard.title = 'Software Developer';
+      vCard.role = 'Software Development';
+      vCard.note = 'Notes on Eric';
+      vCard.email = 'hire@rui-oliveira.com';
+      vCard.source = 'https://www.rui-oliveira.com/vcard/work';
+      vCard.cellPhone = ['312-555-1414'];
+
+      vCard.photo.attachFromUrl('/path/to/file.jpeg');
       break;
 
     case "vip":
       // Set properties that will only be in "vip" card
+      vCard.namePrefix = 'Mr.';
+      vCard.title = 'Software Developer';
+      vCard.role = 'Software Development';
+      vCard.note = 'Notes on Eric';
+      vCard.birthday = new Date(1995, 12, 19);
+      vCard.anniversary = new Date(1995, 12, 19);
+      vCard.email = 'rui.f.oliveira.95@gmail.com';
+      vCard.source = 'https://www.rui-oliveira.com/vcard/vip';
+      vCard.cellPhone = [
+        '312-555-1414',
+        '+351934272501'
+      ];
+
+      vCard.photo.attachFromUrl('/path/to/file.jpeg');
       break;
 
     default:
