216d31a8e6ffdc3de8945868eb3862c38c284bc7
rui1999 <rui.f.oliveira.95@gmail.com>
2022-08-09 00:27:55 +0200

    automatic return to main page affter

--- client/src/components/vcard/Vcard.jsx
+++ client/src/components/vcard/Vcard.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import { useNavigate } from 'react-router-dom'
 
 const server = process.env.REACT_APP_API;
 
 export default function Vcard({ type }) {
   const vCardLink = useRef(null);
   const [fileUrl, setFileUrl] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`${server}/vcard/${type}`)
@@ -15,10 +18,16 @@ export default function Vcard({ type }) {
         setFileUrl(url);
         vCardLink.current?.click();
         URL.revokeObjectURL(url);
+        setTimeout(() => {
+          navigate('/');
+        }, 5000);
       });
   }, []);
 
   const filename = `${type}_vcard.vcf`;
+
+
+
   return (
     <a ref={vCardLink} href={fileUrl} download={filename}>Download Vcard</a>
   )
