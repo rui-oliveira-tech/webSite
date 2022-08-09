7f77400214f77899a95e07a3436c49b343487dfd
rui1999 <rui.f.oliveira.95@gmail.com>
2022-08-09 02:11:44 +0200

    test

--- routes/vcard.js
+++ routes/vcard.js
@@ -1,6 +1,5 @@
 const vcardRouter = require('express').Router();
 const vCardsJS = require('vcards-js');
-const config = require('../config');
 
 vcardRouter.get('/:type', function (req, res, next) {
   const { type } = req.params;
@@ -14,17 +13,12 @@ vcardRouter.get('/:type', function (req, res, next) {
     res.status(500).json({ message: "Could not create requested vcard!" });
   }
 });
-
-
-
 function getVcard(type, fileName) {
   var vCard = vCardsJS();
-  if (![config.type_0, config.type_1].includes(type)) return null;
-
+  if (!["work", "vip"].includes(type)) return null;
   // Create a new vCard
   const vcard = vCardsJS();
   const outputFile = `${__dirname}/${fileName}`;
-
   // Set common properties
   vCard.firstName = 'Rui';
   vCard.lastName = 'Oliveira';
@@ -33,54 +27,44 @@ function getVcard(type, fileName) {
   vCard.socialUrls['linkedIn'] = 'https://www.linkedin.com/in/rui-oliveira--tech/';
   vCard.socialUrls['github'] = 'https://github.com/rui-oliveira-tech/';
   vCard.socialUrls['instagram'] = 'https://www.instagram.com/rui_oliveira_tech/';
-
-
-
-
   vCard.logo.attachFromUrl('https://www.rui-oliveira.com/logo_vcard.png', 'PNG');
-
   switch (type) {
-    case config.type_0:
+    case "work":
       // Set properties that will only be in "work" card
-      vCard.namePrefix = 'Sr. ';
+      vCard.namePrefix = 'Sr.';
       vCard.title = 'Industrial electrician';
       vCard.role = 'Industrial electrician';
       vCard.note = 'An industrial electrician with 5+ years experience looking for job opportunities.';
       vCard.email = 'hire@rui-oliveira.com';
-      vCard.source = `https://www.rui-oliveira.com/vcard/${config.type_0}`;
+      vCard.source = 'https://www.rui-oliveira.com/vcard/work';
       vCard.cellPhone = ['+32474127175'];
 
       vCard.photo.attachFromUrl('https://www.rui-oliveira.com/workFoto_vcard.jpg', 'JPG');
       break;
 
-    case config.type_1:
-      let birthdayArray = config.birthday.split("-");
-
+    case "vip":
       // Set properties that will only be in "vip" card
-      vCard.namePrefix = config.nameprefix;
-      vCard.title = config.title;
-      vCard.role = config.role;
-      vCard.note = config.note;
-      vCard.birthday = new Date(Number(birthdayArray[0]), Number(birthdayArray[1]), Number(birthdayArray[2]));
-      vCard.anniversary = new Date(Number(birthdayArray[0]), Number(birthdayArray[1]), Number(birthdayArray[2]));
-      vCard.email = config.email;
-      vCard.source = `https://www.rui-oliveira.com/vcard/${config.type_1}`;
+      vCard.namePrefix = 'Besty.';
+      vCard.title = 'industrial electrician';
+      vCard.role = 'Industrial electrician';
+      vCard.note = 'Your best friend';
+      vCard.birthday = new Date(1995, 12, 19);
+      vCard.anniversary = new Date(1995, 12, 19);
+      vCard.email = 'rui.f.oliveira.95@gmail.com';
+      vCard.source = 'https://www.rui-oliveira.com/vcard/vip';
       vCard.cellPhone = [
         '+32474127175',
-        config.cellphone
+        '+351934272501'
       ];
 
-
-      vCard.photo.attachFromUrl(config.photo, 'JPG');
+      vCard.photo.attachFromUrl('https://www.rui-oliveira.com/workFoto_vcard.jpg', 'JPG');
       break;
 
     default:
       break;
   }
-
   // Save file
   vCard.saveToFile(outputFile);
   return outputFile;
 }
-
 module.exports = vcardRouter;
