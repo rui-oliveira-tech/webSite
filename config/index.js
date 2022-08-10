82aa55464efee252fcd071f8ac4473c7590efab5
rui1999 <rui.f.oliveira.95@gmail.com>
2022-08-11 00:42:29 +0200

    server Wakeup

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3001,
+    dyno_url: process.env.DYNO_URL,
     origin: [
         '[rui-oliveira.netlify.app]',
         'http://www.rui-oliveira.com',
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const server = require('./server');
-const { port } = require('./config');
+const { port, dyno_url } = require('./config');
+const wakeDyno = require("woke-dyno");
 
 server.listen(port, () => {
+  wakeDyno({
+    url: dyno_url,  // url string
+    interval: 60000 * 29, // interval in milliseconds (1 minute in this example)
+    startNap: [1, 0, 0, 0], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
+    endNap: [6, 59, 59, 999] // time to wake up again, in UTC (09:59:59.999 in this example)
+  }).start();
   // eslint-disable-next-line no-console
   console.log(`Listening at port ${port}`);
 });
