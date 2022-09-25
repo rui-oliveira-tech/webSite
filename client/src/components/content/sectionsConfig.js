94b74951dd8923ac7cc49b2aac8c8d5ae9130f67
rui1999 <rui.f.oliveira.95@gmail.com>
2022-09-25 03:39:55 +0200

    ✏️ Edited text of Other things

--- client/src/components/content/sectionsConfig.js
+++ client/src/components/content/sectionsConfig.js
@@ -7,6 +7,9 @@ import ExperienceImg from "../../images/ExperienceImg.jpg";
 import ProjectsImg from "../../images/ProjectsImg.jpg";
 import OthersImg from "../../images/OthersImg.jpg";
 
+import TraveledMap from "../../images/TraveledMap.jpg";
+
+
 const education = [
     {
         title: "Driving course of electric forklifts, combustion and gas",
@@ -209,7 +212,7 @@ const other = [
         title: "Europe tour",
         startDate: "7 Feb 2021",
         endDate: "Present",
-        description: "My house.",
+        description: "Countries I visited.",
     },
 ];
 
@@ -435,6 +438,7 @@ const sections = [
                             <p className="light text medium textSpacement">{degree.description}</p>
                         </React.Fragment>
                     ))}
+                    <img src={TraveledMap} className="frontImage" alt="Countries i visited" />
                 </div>
             </div>
         ),
@@ -442,3 +446,95 @@ const sections = [
 ];
 
 export default sections;
+
+
+{/* <script src="https://www.amcharts.com/lib/3/ammap.js" type="text/javascript"></script>
+<script src="https://www.amcharts.com/lib/3/maps/js/worldHigh.js" type="text/javascript"></script>
+<script src="https://www.amcharts.com/lib/3/themes/dark.js" type="text/javascript"></script>
+<div id="mapdiv" style="width: 1000px; height: 450px;"></div>
+<div style="width: 1000px; font-size: 70%; padding: 5px 0; text-align: center; background-color: #535364; margin-top: 1px; color: #B4B4B7;"><a href="https://www.amcharts.com/visited_countries/" style="color: #B4B4B7;">Create your own visited countries map</a> or check out the <a href="https://www.amcharts.com/" style="color: #B4B4B7;">JavaScript Charts</a>.</div>
+<script type="text/javascript">
+var map = AmCharts.makeChart("mapdiv",{
+type: "map",
+theme: "dark",
+projection: "mercator",
+panEventsEnabled : true,
+backgroundColor : "#535364",
+backgroundAlpha : 1,
+zoomControl: {
+zoomControlEnabled : true
+},
+dataProvider : {
+map : "worldHigh",
+getAreasFromMap : true,
+areas :
+[
+	{
+		"id": "AT",
+		"showAsSelected": true
+	},
+	{
+		"id": "BE",
+		"showAsSelected": true
+	},
+	{
+		"id": "HR",
+		"showAsSelected": true
+	},
+	{
+		"id": "CZ",
+		"showAsSelected": true
+	},
+	{
+		"id": "FR",
+		"showAsSelected": true
+	},
+	{
+		"id": "DE",
+		"showAsSelected": true
+	},
+	{
+		"id": "HU",
+		"showAsSelected": true
+	},
+	{
+		"id": "IT",
+		"showAsSelected": true
+	},
+	{
+		"id": "LU",
+		"showAsSelected": true
+	},
+	{
+		"id": "PT",
+		"showAsSelected": true
+	},
+	{
+		"id": "SK",
+		"showAsSelected": true
+	},
+	{
+		"id": "SI",
+		"showAsSelected": true
+	},
+	{
+		"id": "ES",
+		"showAsSelected": true
+	},
+	{
+		"id": "SE",
+		"showAsSelected": true
+	}
+]
+},
+areasSettings : {
+autoZoom : true,
+color : "#B4B4B7",
+colorSolid : "#84ADE9",
+selectedColor : "#84ADE9",
+outlineColor : "#666666",
+rollOverColor : "#9EC2F7",
+rollOverOutlineColor : "#000000"
+}
+});
+</script> */}
