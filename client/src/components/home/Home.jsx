2a78b7125acd443384c7440ee29bc7c945255dc2
rui1999 <rui.f.oliveira.95@gmail.com>
2022-10-07 03:00:35 +0200

    🔥 added button CV download

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import FileSaver from 'file-saver';
 import "./Home.scss";
+import "./CvButton.scss";
 import main_Img from "../../images/main_Img.jpg";
 
 
@@ -11,9 +13,20 @@ export default function Home({ allPages }) {
       block: "start",
     });
   }
+  const saveFile = () => {
+    FileSaver.saveAs(
+      process.env.PUBLIC_URL + "/resource/CV - EN - Rui Oliveira.pdf",
+      "CV - EN - Rui Oliveira.pdf");
+  };
   return (
     <section className="scroll_to home">
       <header>
+        <button class="learn-more" onClick={saveFile}>
+          <span class="circle" aria-hidden="true">
+            <span class="icon arrow"></span>
+          </span>
+          <span class="button-text"> Get My CV</span>
+        </button>
         <figure>
           <img src={main_Img} className="frontImage" alt="Rui Oliveira" />
         </figure>
@@ -24,7 +37,7 @@ export default function Home({ allPages }) {
         <h3 data-texteffect="Residencial Electrician" className="subTitle second">Residencial Electrician</h3>
         <h3 data-texteffect="Schematic Projecting" className="subTitle third">Schematic Projecting</h3>
       </main>
-      
+
       <footer className="scrollArrow" onClick={scrollDown}>
         <span></span>
         <span></span>
