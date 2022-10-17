76471112a4535e49e0742586a372ab4a87195ca3
rui1999 <rui.f.oliveira.95@gmail.com>
2022-10-17 18:21:19 +0200

    🔥 added languages

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import { useTranslation } from 'react-i18next'
 import ReactGA from 'react-ga4';
 import "./Footer.scss"
 
 import Button from "../Button/Button"
 
 export function Footer() {
+  const { t } = useTranslation();
   const onClick = (type) => () => {
     ReactGA.event({
       action: "social media",
@@ -18,7 +20,7 @@ export function Footer() {
 
       <a className="icon" target="_blank" rel="noreferrer" onClick={onClick("instagram")} href="https://api.whatsapp.com/send/?phone=32474127175&text&type=phone_number&app_absent=0"><i className="icon-whatsapp" /></a>
 
-      <Button className="from-center" onClick={onClick("mail")} href="mailto:hire@rui-oliveira.com">Hire Me</Button>
+      <Button className="from-center" onClick={onClick("mail")} href="mailto:hire@rui-oliveira.com">{t('footer.mailButton')}</Button>
 
       <a className="icon" target="_blank" rel="noreferrer" onClick={onClick("github")} href="https://github.com/RuiOliveira-Electrician/"><i className="icon-github" /></a>
 
