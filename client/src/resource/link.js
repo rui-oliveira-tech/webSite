106d3aa66c26180c1634c0f13b3410bc1b2236f5
rui1999 <rui.f.oliveira.95@gmail.com>
2023-01-25 02:40:43 +0100

    🔨 changed files location

---
+++ client/src/resource/link.js
@@ -0,0 +1,47 @@
+export default function GetLink(type) {
+  switch (type) {
+    case "facebook":
+      return "https://www.facebook.com/RuiOliveira.Electrician/";
+    case "instagram":
+      return "https://www.instagram.com/RuiOliveira_Electrician/";
+    case "linkedin":
+      return "https://www.linkedin.com/in/RuiOliveira-Electrician/";
+    case "github":
+      return "https://github.com/RuiOliveira-Electrician/";
+    case "whatsapp":
+      return "https://api.whatsapp.com/send/?phone=32474127175&text&type=phone_number&app_absent=0";
+    case "mail":
+      return "mailto:hire@rui-oliveira.com";
+    /*  case "google":
+     return "Google";
+      case "vdab":
+     return "VDAB";
+   case "twitter":
+     return "Twitter"; */
+    default:
+      return "https://www.rui-oliveira.com/";
+  }
+}
+
+export function GetType({ type }) {
+  switch (type) {
+    case "face":
+      return "Facebook";
+    case "insta":
+      return "Instagram";
+    case "linkedin":
+      return "Linkedin";
+    case "github":
+      return "Github";
+    case "google":
+      return "Google";
+    case "mail":
+      return "Mail";
+    case "vdab":
+      return "VDAB";
+    case "twitter":
+      return "Twitter";
+    default:
+      return "other";
+  }
+}
---
+++ client/src/resource/programingLanguages.js
@@ -0,0 +1,102 @@
+import Arduino_Svg from "../images/Programing_Languages/Arduino_Svg.svg";
+import Cpp_Svg from "../images/Programing_Languages/Cpp_Svg.svg";
+
+import Css_Svg from "../images/Programing_Languages/Css_Svg.svg";
+import Html_Svg from "../images/Programing_Languages/Html_Svg.svg";
+import Js_Svg from "../images/Programing_Languages/Js_Svg.svg";
+import NodeJs_Svg from "../images/Programing_Languages/NodeJs_Svg.svg";
+
+
+
+import Lader_Svg from "../images/Programing_Languages/Lader_Svg.svg";
+
+import Git_Svg from "../images/Programing_Languages/Git_Svg.svg";
+import Vsc_Svg from "../images/Programing_Languages/Vsc_Svg.svg";
+import Linux_Svg from "../images/Programing_Languages/Linux_Svg.svg";
+import Windows_Svg from "../images/Programing_Languages/Windows_Svg.svg";
+
+
+export const programingLanguages = [
+    [
+        {
+            id: 1,
+            title: "Industrial",
+        },
+        {
+            id: 2,
+            title: "",
+
+        },
+        {
+            id: 3,
+            title: "",
+
+        },
+    ],
+    [
+        {
+            title: "Ladder",
+            imgLink: Lader_Svg,
+            website: "https://ladderlogicworld.com/",
+        },
+        /*    {
+               title: "FrequencyInverters",
+               imgLink: FrequencyInverters_Img,
+               website: "",
+           }, */
+        {
+            title: "Arduino",
+            imgLink: Arduino_Svg,
+            website: "https://github.com/rui-oliveira-tech/webSite/",
+        },
+    ],
+    [
+        {
+            title: "C++",
+            imgLink: Cpp_Svg,
+            website: "https://www.w3schools.com/cpp/",
+        },
+        {
+            title: "Css",
+            imgLink: Css_Svg,
+            website: "https://www.w3schools.com/css/",
+        },
+        {
+            title: "Html",
+            imgLink: Html_Svg,
+            website: "https://www.w3.org/html/",
+        },
+        {
+            title: "Js",
+            imgLink: Js_Svg,
+            website: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/",
+        },
+        {
+            title: "NodeJs",
+            imgLink: NodeJs_Svg,
+            website: "https://nodejs.org/",
+        },
+    ],
+    [
+        {
+            title: "Git",
+            imgLink: Git_Svg,
+            website: "https://git-scm.com/",
+        },
+        {
+            title: "Visual Studio Code",
+            imgLink: Vsc_Svg,
+            website: "https://code.visualstudio.com/",
+        },
+        {
+            title: "Linux",
+            imgLink: Linux_Svg,
+            website: "https://www.linux.org/",
+        },
+        {
+            title: "Windows",
+            imgLink: Windows_Svg,
+            website: "https://www.microsoft.com/en-gb/windows/",
+        },
+    ],
+];
