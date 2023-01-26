79f88580380b4e0f7910f8edda7abf9555a7f950
rui1999 <rui.f.oliveira.95@gmail.com>
2023-01-26 04:20:57 +0100

    ⬆️ update pages

---
+++ client/src/resource/pages.js
@@ -0,0 +1,17 @@
+import Homepage from '../Homepage';
+import Portfolio from "../Portfolio";
+
+export const pageList = [
+  {
+    url: '',
+    Component: Portfolio
+  },
+  /* {
+    url: '',
+    Component: Homepage
+  },
+  {
+    url: 'portefolio',
+    Component: Portfolio
+  }, */
+]
