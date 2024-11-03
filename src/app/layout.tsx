ccdd5191075cf6739f3f1ed45c43d33af5a6d73c
rui1999 <rui.f.oliveira.95@gmail.com>
2024-11-03 19:10:08 +0100

    🔨  fix bugs 1 upload netlify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,35 @@
 import React from "react";
-import { getLocale, getMessages } from "next-intl/server";
+/* import { getLocale, getMessages } from "next-intl/server";
 
 import RootLayout from "./RootLayout";
 import NavigationBar from "@/components/layout/navigationBar/NavigationBar";
-import Footer from "@/components/layout/footer/Footer";
+import Footer from "@/components/layout/footer/Footer"; */
 
 import "@/assets/styles/globalVar.scss";
 import "@/assets/styles/global.css";
 
 interface ILayoutProps {
   children: React.ReactNode;
-  params: {
-    locale: string;
-  };
 }
 
 export default async function Layout(props: ILayoutProps) {
-  const locale = await getLocale();
-  const messages = await getMessages();
+  /*  const locale = await getLocale();
+  const messages = await getMessages(); */
 
   return (
-    <RootLayout locale={locale} messages={messages}>
+    <>
+     <html lang={"en"}>
+      <body>
+        aaa
+      </body>
+     </html>
+      {/* <RootLayout locale={locale} messages={messages}>
       <div className="wideGrid">
         <NavigationBar locale={locale} />
         {props.children}
         <Footer />
       </div>
-    </RootLayout>
+    </RootLayout> */}
+    </>
   );
 }
