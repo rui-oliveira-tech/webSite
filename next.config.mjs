e63ae649d029874fed8b106ba00ef32e843d5188
rui1999 <rui.f.oliveira.95@gmail.com>
2024-11-03 19:30:43 +0100

    Update Next.js config to use static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+    output: "export",
+};
+
 export default withNextIntl(nextConfig);
