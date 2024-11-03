b181db87e6cbf89025b981e6ca975a3bc2f4d4df
rui1999 <rui.f.oliveira.95@gmail.com>
2024-11-03 21:53:05 +0100

    🔨  1 upload on NextJs

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useTranslations } from "next-intl";
+// import { useTranslations } from "next-intl";
 import "./Home.scss";
 
 import homeImg from "@/assets/images/home/main.jpg";
@@ -9,7 +9,8 @@ import homeImg from "@/assets/images/home/main.jpg";
 interface IHomeProps {}
 
 export function Home(props: IHomeProps) {
-  const t = useTranslations("");
+  // const t = useTranslations("");
+
   return (
     <section className="have_footer have_NavigationBar home">
       <header>
@@ -24,24 +25,24 @@ export function Home(props: IHomeProps) {
         </figure>
       </header>
       <main>
-        <h1 className="title">{t("home.title")}</h1>
+        <h1 className="title">{/* t("home.title") */} Home Title</h1>
         <h2
-          data-texteffect={t("home.subTitle.first")}
-          className={"subTitle first "}
+          data-texteffect={/* t("home.subTitle.first") */}
+          className="subTitle first"
         >
-          {t("home.subTitle.first")}
+          {/* t("home.subTitle.first") */} Sub Title First
         </h2>
         <h3
-          data-texteffect={t("home.subTitle.second")}
-          className={"subTitle second "}
+          data-texteffect={/* t("home.subTitle.second") */}
+          className="subTitle second"
         >
-          {t("home.subTitle.second")}
+          {/* t("home.subTitle.second") */} Sub Title Second
         </h3>
         <h3
-          data-texteffect={t("home.subTitle.third")}
-          className={"subTitle third "}
+          data-texteffect={/* t("home.subTitle.third") */}
+          className="subTitle third"
         >
-          {t("home.subTitle.third")}
+          {/* t("home.subTitle.third") */} Sub Title Third
         </h3>
       </main>
     </section>
