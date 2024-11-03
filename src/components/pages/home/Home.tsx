1161b71e27247d7299fe84d6bc7a6ccbcf9f697a
rui1999 <rui.f.oliveira.95@gmail.com>
2024-11-03 22:46:05 +0100

    🔨  1 upload on NextJs

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -13,7 +13,38 @@ export function Home(props: IHomeProps) {
 
   return (
     <section className="have_footer have_NavigationBar home">
-     ss
+      <header>
+        <figure>
+         {/*  <Image
+            src={homeImg}
+            className="frontImage"
+            width={500}
+            height={500}
+            alt="Rui Oliveira"
+          /> */}
+        </figure>
+      </header>
+      <main>
+        <h1 className="title">{/* t("home.title") */} Home Title</h1>
+        <h2
+          className="subTitle first"
+          // data-texteffect={t("home.subTitle.first")} // Uncomment to use translations
+        >
+          {/* t("home.subTitle.first") */} Sub Title First
+        </h2>
+        <h3
+          className="subTitle second"
+          // data-texteffect={t("home.subTitle.second")} // Uncomment to use translations
+        >
+          {/* t("home.subTitle.second") */} Sub Title Second
+        </h3>
+        <h3
+          className="subTitle third"
+          // data-texteffect={t("home.subTitle.third")} // Uncomment to use translations
+        >
+          {/* t("home.subTitle.third") */} Sub Title Third
+        </h3>
+      </main>
     </section>
   );
 }
