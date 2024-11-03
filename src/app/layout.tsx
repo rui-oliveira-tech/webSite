2583e84ce8c32ca4a8a045839da8dd1ab74c5593
rui1999 <rui.f.oliveira.95@gmail.com>
2024-11-03 23:46:57 +0100

    🔨  1 upload on NextJs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-/* import { getLocale, getMessages } from "next-intl/server";
+import { getLocale, getMessages } from "next-intl/server";
 
 import RootLayout from "./RootLayout";
 import NavigationBar from "@/components/layout/navigationBar/NavigationBar";
-import Footer from "@/components/layout/footer/Footer"; */
+import Footer from "@/components/layout/footer/Footer";
 
 import "@/assets/styles/globalVar.scss";
 import "@/assets/styles/global.css";
@@ -13,23 +13,21 @@ interface ILayoutProps {
 }
 
 export default async function Layout(props: ILayoutProps) {
-  /*  const locale = await getLocale();
-  const messages = await getMessages(); */
+  const locale = await getLocale();
+  const messages = await getMessages();
 
   return (
     <>
-     <html lang={"en"}>
-      <body>
-        aaa
-      </body>
-     </html>
-      {/* <RootLayout locale={locale} messages={messages}>
-      <div className="wideGrid">
-        <NavigationBar locale={locale} />
-        {props.children}
-        <Footer />
-      </div>
-    </RootLayout> */}
+      <html lang={"en"}>
+        <body>aaa</body>
+      </html>
+      <RootLayout locale={locale} messages={messages}>
+        <div className="wideGrid">
+          <NavigationBar locale={locale} />
+          {props.children}
+          <Footer />
+        </div>
+      </RootLayout>
     </>
   );
 }
--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,48 +1,48 @@
 "use client";
 
-/* import Image from "next/image"; */
-// import { useTranslations } from "next-intl";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
 import "./Home.scss";
 
-/* import homeImg from "@/assets/images/home/main.jpg"; */
+import homeImg from "@/assets/images/home/main.jpg";
 
 interface IHomeProps {}
 
 export function Home(props: IHomeProps) {
-  // const t = useTranslations("");
+  const t = useTranslations("");
 
   return (
     <section className="have_footer have_NavigationBar home">
       <header>
         <figure>
-         {/*  <Image
+          <Image
             src={homeImg}
             className="frontImage"
             width={500}
             height={500}
             alt="Rui Oliveira"
-          /> */}
+          />
         </figure>
       </header>
       <main>
-        <h1 className="title">{/* t("home.title") */} Home Title</h1>
+        <h1 className="title"> {t("home.title")}</h1>
         <h2
           className="subTitle first"
-          // data-texteffect={t("home.subTitle.first")} // Uncomment to use translations
+          data-texteffect={t("home.subTitle.first")}
         >
-          {/* t("home.subTitle.first") */} Sub Title First
+          {t("home.subTitle.first")}
         </h2>
         <h3
           className="subTitle second"
-          // data-texteffect={t("home.subTitle.second")} // Uncomment to use translations
+          data-texteffect={t("home.subTitle.second")}
         >
-          {/* t("home.subTitle.second") */} Sub Title Second
+          {t("home.subTitle.second")}
         </h3>
         <h3
           className="subTitle third"
-          // data-texteffect={t("home.subTitle.third")} // Uncomment to use translations
+          data-texteffect={t("home.subTitle.third")}
         >
-          {/* t("home.subTitle.third") */} Sub Title Third
+          {t("home.subTitle.third")}
         </h3>
       </main>
     </section>
